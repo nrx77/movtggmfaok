0d92f3c8d328dfdc45ce21ba61d6542dcd35803f
EX69 <cosadiz69@gmail.com>
2021-12-23 00:08:37 +0800

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -162,7 +162,7 @@ async def advantage_spoll_choker(bot, query):
         k = (movie, files, offset, total_results)
         await auto_filter(bot, query, k)
     else:
-        k = await query.message.edit(f'⚠️ Hey, {query.from_user.first_name}! Filmnya tidak ada atau belum di up sama admin\nSilahkan Ke Grub Movie Request⚡')
+        k = await query.message.edit(f'⚠️ Hey, {query.from_user.first_name}! Filmnya tidak ada atau belum di up sama admin\n\nSilahkan Req di <a href='https://t.me/+iXFDwdXc_FswZTNl'>Movie Request⚡ 👈</a>')
         await asyncio.sleep(10)
         await k.delete()
     
