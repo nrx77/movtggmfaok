e1261792bb15d839dfc145b52a930b11a81bf067
nrx77 <97866445+nrx77@users.noreply.github.com>
2022-03-02 23:59:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/nrx77/movtgg /movtgg
-cd /movtgg
+git clone -b master https://github.com/nrx77/movtggmfaok /movtggmfaok
+cd /movtggmfaok
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
